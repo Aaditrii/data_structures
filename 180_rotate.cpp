b97556d34ff80bf74d92f1fe28ff6e5eb23896af
Aaditrii <aaditri1002@gmail.com>
2019-08-04 10:07:22 +0530

    rotating the array by 180 degrees

--- 180_rotate.cpp
+++ 180_rotate.cpp
@@ -1,22 +1,32 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int n;
-	cin>>n;
-	int arr[n][n];
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			scanf("%d",&arr[i][j]);
+void rotate_by_180(int arr[30][30],int n){
+	int temp=0;
+	for(int i=0;i<=n/2;i++){
+		for(int j=n-1;j>=i;j--){
+			temp=arr[i][n-j-1];
+			arr[i][n-j-1]=arr[n-i-1][j];
+	        	arr[n-i-1][j]=temp;
 		}
 	}
-	cout<<"rotate by 180 degrees"<<endl;
-	for(int i=n-1;i>=0;i--){
-		for(int j=n-1;j>=0;j--)
-		{
-			cout<<arr[i][j]<<"\t";
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<arr[i][j]<<" ";
 		}
 		cout<<endl;
 	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+        int arr[30][30];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>arr[i][j];
+		}
+	}
+	rotate_by_180(arr,n);
 	return 0;
 }
 
