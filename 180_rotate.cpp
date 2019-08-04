fca1c52d05279b3cfc2417132424e7e3543438ae
Aaditrii <aaditri1002@gmail.com>
2019-08-04 16:31:12 +0530

    rotating a matrix by 180 degrees

--- 180_rotate.cpp
+++ 180_rotate.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 void rotate_by_180(int arr[30][30],int n){
 	int temp=0;
-	for(int i=0;i<=n/2;i++){
-		for(int j=n-1;j>=i;j--){
+	for(int i=0;i<n/2;i++){
+		for(int j=n-1;j>=0;j--){
 			temp=arr[i][n-j-1];
 			arr[i][n-j-1]=arr[n-i-1][j];
 	        	arr[n-i-1][j]=temp;
