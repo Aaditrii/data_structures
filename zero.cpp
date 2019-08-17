5ab78f0b85e6f51ba7ee3d0142a0f763990d5c2c
Aaditrii <aaditri1002@gmail.com>
2019-08-17 10:24:16 +0530

    segregation of zeroes at the start and ones at the end

---
+++ zero.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void swap(int *num1,int *num2){
+	int t;
+	t=*num1;
+	*num1=*num2;
+	*num2=t;
+}
+void dutch_sort(int arr[],int k){
+	int low=0;
+	int mid=0;
+	int high=k-1;
+	while(mid<=high){
+		switch(arr[mid]){
+			case 0:
+				swap(&arr[low++],&arr[mid++]);
+				break;
+			case 1:
+				mid++;
+				break;
+		}
+	}
+}
+int main(int argc,const char *argv[]){
+	int arr[100],k=0;
+	for(int i=1;i<argc;i++)
+		arr[k++]=atoi(argv[i]);
+	dutch_sort(arr,k);
+	for(int i=0;i<k;i++)
+		cout<<arr[i]<<" ";
+	return 0;
+}
