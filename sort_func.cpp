2c212fd6f61a40e46f1b9f22cf106a0e35affd2b
Aaditrii <aaditri1002@gmail.com>
2019-08-17 10:36:28 +0530

    sort function in stl in ascending order

---
+++ sort_func.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstdlib>
+using namespace std;
+int main(){
+	int arr[]={1,5,8,9,10,3,4,6,7};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,arr+n);
+	for(int i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+	return 0;
+}
+
