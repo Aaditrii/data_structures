43973ff70496abe834e7c9386e8137c521030417
Aaditrii <aaditri1002@gmail.com>
2019-08-30 11:05:55 +0530

    apllication of quick sort by using low indexing element as pivot

---
+++ quick_sort1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+void swap(int *num1,int *num2){
+	int t;
+	t=*num1;
+	*num1=*num2;
+	*num2=t;
+}
+int partition(int arr[100],int low,int high){
+int	 pivot=arr[low];
+	int i=high+1;
+	for(int j=high;j>low;j--){
+		if(arr[j]>=pivot){
+			i--;
+			swap(&arr[j],&arr[i]);
+		}
+	}
+	swap(&arr[i-1],&arr[low]);
+	return(i-1);
+}
+void quick_sort(int arr[100],int low,int high){
+	if(low<high){
+		int pi=partition(arr,low,high);
+		quick_sort(arr,low,pi-1);
+		quick_sort(arr,pi+1,high);
+	}
+}
+int main(int argc,const char *argv[]){
+	int arr[100],k=0;
+	for(int i=1;i<argc;i++)
+		arr[k++]=atoi(argv[i]);
+	srand(time(0));
+	quick_sort(arr,0,k-1);
+	for(int i=0;i<k;i++)
+		cout<<arr[i]<<" ";
+	return 0;
+}
+
